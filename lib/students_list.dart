3f7d9f152e7d8de0ef1d13c7d378507b65a40e1e
Aurelio <auesra16@gmail.com>
2023-10-29 19:00:28 +0100

    Pagina home

---
+++ lib/students_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StudentsList extends StatelessWidget {
+  const StudentsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
