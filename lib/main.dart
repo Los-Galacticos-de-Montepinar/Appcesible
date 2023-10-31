dbb4f1f286594081b11fdd8e2f26c13aaaeba415
jesusarsan <jesus02@correo.ugr.es>
2023-10-31 16:28:29 +0100

    Code Update

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  List<String> nombresUsuarios = [];
+  List<Map<String, String>> usersList = [];
   int currentIndex = 0;
 
   Future<void> fetchDataFromServer() async {
@@ -25,9 +25,22 @@ class _MainAppState extends State<MainApp> {
       final data = response.body;
 
       if (data.isNotEmpty) {
-        final nombres = data.split(", ");
+        final users = data.split(", ");
+
         setState(() {
-          nombresUsuarios = nombres; // Asigna los nombres de usuario
+          int numUsers = users.length;
+          String first = '', last = '';
+
+          for (int i = 0; i < numUsers; i++) {
+            final user = users.elementAt(i).split('=');
+            first = user.first;
+            last = user.last;
+            Map<String, String> userMap = {};
+            userMap['id'] = first;
+            userMap['name'] = last;
+
+            usersList.add(userMap);
+          }
         });
       } else {
         // La respuesta está vacía
@@ -47,9 +60,9 @@ class _MainAppState extends State<MainApp> {
 
   // Cambia usuario
   void changeUser(int increment) {
-    if (nombresUsuarios.isNotEmpty) {
+    if (usersList.isNotEmpty) {
       setState(() {
-        currentIndex = (currentIndex + increment) % nombresUsuarios.length;
+        currentIndex = (currentIndex + increment) % usersList.length;
       });
     }
   }
@@ -77,49 +90,77 @@ class _MainAppState extends State<MainApp> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              color: Colors.grey[500],
+            // Padding for the back button
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20.0,
+                top: 20.0,
+                bottom: 30.0,
+                right: 0.0,
+              ),
+              child: Align(
+                  alignment: Alignment
+                      .topLeft, // Alinea en la esquina superior izquierda
+                  child: SizedBox(
+                    width: 150,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Colors.orange[600], // Cambia el color de fondo
+                        ),
+                        child: const Row(
+                          children: [
+                            Icon(
+                              Icons.subdirectory_arrow_left_outlined,
+                              size: 38,
+                              color: Colors.white,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Volver',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        )),
+                  )),
+            ),
+
+            // User Content
+            SizedBox(
+              // Box where the name is shown
               height: 80,
-              width: 200,
+              width: 250,
               child: Center(
                 child: Text(
-                  nombresUsuarios.isNotEmpty
-                      ? nombresUsuarios[currentIndex]
+                  usersList.isNotEmpty
+                      ? usersList[currentIndex].values.last
                       : "",
                   style: const TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
                 ),
               ),
             ),
+
+            // Container with the profile picture, and the button to
+            // change the user
             Container(
-              color: Colors.yellow,
-              height: 250,
+              margin: const EdgeInsets.only(bottom: 20.0),
+              height: 200,
               width: 350,
               child: Row(
                 children: <Widget>[
-                  ElevatedButton(
-                    onPressed: () {
-                      // Cambiar al nombre anterior
-                      changeUser(-1);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                    ),
-                    child: const Icon(Icons.arrow_back),
-                  ),
-                  const SizedBox(width: 20),
-                  Expanded(
-                    child: Container(
-                      color: Colors.grey,
-                      height: 200,
-                      width: 50,
-                    ),
-                  ),
-                  const SizedBox(width: 20),
+                  // Elevated Button to the previous user
                   ElevatedButton(
                     onPressed: () {
                       // Cambiar al nombre siguiente
@@ -127,20 +168,115 @@ class _MainAppState extends State<MainApp> {
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.black,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          bottomLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(0.0),
+                          bottomRight: Radius.circular(0.0),
+                        ),
+                      ),
                     ),
-                    child: const Icon(Icons.arrow_forward),
+                    child: const Icon(
+                      Icons.arrow_back,
+                      size: 38, // Ajusta el tamaño según tus necesidades
+                      color: Colors.white, // Cambia el color de la flecha
+                    ),
+                  ),
+
+                  // Box transparent between the button and the profile picture
+                  const SizedBox(width: 20),
+
+                  // Profile Picture box
+                  Expanded(
+                    child: Container(
+                      width: 200,
+                      height: 170,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 5.0,
+                        ),
+                      ),
+                      child: ClipOval(
+                        child: Image.asset(
+                          'images/alonso.png',
+                          width: 200,
+                          height: 200,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // Box transparent between the button and the profile picture
+                  const SizedBox(width: 20),
+
+                  // Elevated Button to the next user
+                  ElevatedButton(
+                    onPressed: () {
+                      // Cambiar al nombre siguiente
+                      changeUser(1);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(0.0),
+                          bottomLeft: Radius.circular(0.0),
+                          topRight: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0),
+                        ),
+                      ),
+                    ),
+                    child: const Icon(
+                      Icons.arrow_forward,
+                      size: 38, // Ajusta el tamaño según tus necesidades
+                      color: Colors.white, // Cambia el color de la flecha
+                    ),
                   ),
                 ],
               ),
             ),
-            GestureDetector(
-              onTap: onPressedButton,
-              child: Image.asset(
-                'images/volver.png',
-                height: 150,
-                width: 240,
+
+            // Button to select the profile shown
+            SizedBox(
+              width: 210,
+              height: 60,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange[600],
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      bottomLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                      bottomRight: Radius.circular(20.0),
+                    ),
+                  ),
+                ),
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.check_circle, // Ícono de flecha hacia adelante
+                      size: 33,
+                      color: Colors.white, // Color del ícono
+                    ),
+                    SizedBox(width: 12),
+                    Text(
+                      'Seleccionar',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+            )
           ],
         ),
       ),
