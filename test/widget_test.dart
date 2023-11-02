bdbeda2da9110af1027ec36859ee5ead5e9771f5
jesusarsan <jesus02@correo.ugr.es>
2023-11-02 12:46:40 +0100

    Base Responsive

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,10 +10,10 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:appcesible/main.dart';
 
-void main() { 
+void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MainApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
