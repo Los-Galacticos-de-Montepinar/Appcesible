fd45636ff27a3a1081a3ce7de9a659dccfb54a07
pabloqr <pabloquesadarojo02@gmail.com>
2023-11-08 01:20:30 +0100

    minor changes

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -7,10 +7,10 @@ void createUser(UserModel user, String password) async {
   int id = user.id;
   final response = await http.post(
     Uri.http(UserModel.baseAddress, '/user/new'),
-    headers: <String, String> {
+    headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8'
     },
-    body: jsonEncode(<String, dynamic> {
+    body: jsonEncode(<String, dynamic>{
       'userName': user.userName,
       'passwd': password,
       'pfp': user.idProfileImg,
@@ -22,26 +22,22 @@ void createUser(UserModel user, String password) async {
 
   if (response.statusCode == 200) {
     print("Updated user");
-  }
-  else {
+  } else {
     throw Exception('Failed to update User');
   }
 }
 
 // Function that makes a HTTP request to get a User from the server DB
 Future<UserModel> getUserFromId(int id) async {
-  final response = await http.get(
-    Uri.http(UserModel.baseAddress, '/user/$id'),
-    headers: <String, String> {
-      'Content-Type': 'application/json; charset=UTF-8'
-    }
-  );
+  final response = await http.get(Uri.http(UserModel.baseAddress, '/user/$id'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      });
 
   if (response.statusCode == 200) {
     dynamic json = jsonDecode(response.body);
     return UserModel.fromJSON(json);
-  }
-  else {
+  } else {
     throw Exception('Failed to load User');
   }
 }
@@ -51,10 +47,10 @@ void updateUser(UserModel user, String password) async {
   int id = user.id;
   final response = await http.post(
     Uri.http(UserModel.baseAddress, '/user/$id'),
-    headers: <String, String> {
+    headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8'
     },
-    body: jsonEncode(<String, dynamic> {
+    body: jsonEncode(<String, dynamic>{
       'name': user.userName,
       'passwd': password,
       'pfp': user.idProfileImg
@@ -63,8 +59,7 @@ void updateUser(UserModel user, String password) async {
 
   if (response.statusCode == 200) {
     print("Updated user");
-  }
-  else {
+  } else {
     throw Exception('Failed to update User');
   }
 }
