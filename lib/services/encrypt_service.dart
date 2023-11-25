3c4cd718493bfb62669eec84c16d4edaf3aee988
pabloqr <pabloquesadarojo02@gmail.com>
2023-11-25 20:46:06 +0100

    function to send public key to server

--- lib/services/encrypt_service.dart
+++ lib/services/encrypt_service.dart
@@ -5,9 +5,27 @@ import 'package:appcesible/command/encrypt_command.dart';
 
 String _baseAddress = '10.0.2.2:8080';
 
+void getKeyFromServer() async {
+  final response = await http.get(
+    Uri.http(_baseAddress, '/session/public'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8'
+    },
+  );
+
+  print('esperando respuesta');
+
+  if (response.statusCode == 200) {
+    //Map<String, dynamic> json = jsonDecode(response.body);
+    String data = response.body;
+
+    print(data);
+  }
+}
+
 void sendPublicKey() async {
   final response = await http.post(
-    Uri.http(_baseAddress, '/session/public'),
+    Uri.http(_baseAddress, '/session/test'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8'
     },
@@ -15,4 +33,8 @@ void sendPublicKey() async {
       'data': generatePem()
     }),
   );
+}
+
+void main() {
+  getKeyFromServer();
 }
