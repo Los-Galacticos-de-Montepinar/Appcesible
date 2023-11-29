25b8351fc570b35f18325221ee9dc62a9ccaf809
pabloqr <pabloquesadarojo02@gmail.com>
2023-11-29 18:39:51 +0100

    fixes for merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
-import 'package:appcesible/screens/normal_passw.dart';
-import 'package:appcesible/screens/pictograms_passw.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'package:appcesible/screens/select_user.dart';
-import 'package:appcesible/screens/formulario_usuarios.dart';
+import 'package:appcesible/screens/task_list_mobile.dart';
+import 'package:appcesible/screens/create_task_init.dart';
+import 'package:appcesible/screens/home_student_init.dart';
+import 'package:appcesible/screens/formulario_alumno.dart';
 import 'package:appcesible/screens/home_teacher.dart';
-import 'widgets/top_menu.dart';
+import 'package:appcesible/screens/select_user.dart';
+import 'package:appcesible/widgets/new_step.dart';
+
+import 'package:appcesible/widgets/top_menu.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,30 +18,133 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  // SELECCION DE USUARIO
-
-  //@override
-  /*Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(412, 570), // ?
-      builder: (context, child) {
-        return const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: SelectUser(),
-        );
-      },
-    );
-  }*/
-
-  // HOME DEL PROFESOR
+  // SELECCION DE USUARIO !!!!!
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      //debugShowCheckedModeBanner: false,
-      home: TeacherHome(),
+
+    List<MyTaskData> tasks2 = [
+      MyTaskData(
+        imagePath: 'assets/images/gato.png',
+        taskName: 'Hacer ejercicio y hacer los deberes',
+        studentName: 'Juan Perez',
+        state: 'done',
+      ),
+      MyTaskData(
+        imagePath: 'assets/images/gato.png',
+        taskName: 'Estudiar para el examen',
+        studentName: 'Maria Rodriguez',
+        state: 'done',
+      ),
+      MyTaskData(
+        imagePath: 'assets/images/gato.png',
+        taskName: 'Completar proyecto',
+        studentName: 'Carlos Lopez',
+        state: 'not finished',
+      ),
+      MyTaskData(
+        imagePath: 'assets/images/gato.png',
+        taskName: 'Hacer ejercicio',
+        studentName: 'Juan Perez',
+        state: 'done',
+      ),
+      MyTaskData(
+        imagePath: 'assets/images/gato.png',
+        taskName: 'Estudiar para el examen',
+        studentName: 'Maria Rodriguez',
+        state: 'not finished',
+      ),
+      MyTaskData(
+        imagePath: 'assets/images/gato.png',
+        taskName: 'Completar proyecto',
+        studentName: 'Carlos Lopez',
+        state: 'done',
+      ),
+      MyTaskData(
+        imagePath: 'assets/images/gato.png',
+        taskName: 'Hacer ejercicio',
+        studentName: 'Juan Perez',
+        state: 'not finished',
+      ),
+      MyTaskData(
+        imagePath: 'assets/images/gato.png',
+        taskName: 'Estudiar para el examen',
+        studentName: 'Maria Rodriguez',
+        state: 'not done',
+      ),
+      MyTaskData(
+        imagePath: 'assets/images/gato.png',
+        taskName: 'Completar proyecto',
+        studentName: 'Carlos Lopez',
+        state: 'not done',
+      ),
+    ];
+
+    return ScreenUtilInit(
+      designSize: const Size(412, 570),
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: TaskListMobile(tasks: tasks2),
+        );
+      },
     );
   }
+
+  // TASK CREATION
+  
+  //@override
+  //Widget build(BuildContext context) {
+  //  return ScreenUtilInit(
+  //    designSize: const Size(412, 570),
+  //    builder: (context, child) {
+  //      return const MaterialApp(
+  //        debugShowCheckedModeBanner: false,
+  //        home: CreateTaskInit(),
+  //      );
+  //    },
+  //  );
+  //}  
+
+  // HOME USUARIO
+  
+  //@override
+  //Widget build(BuildContext context) {
+  //  return ScreenUtilInit(
+  //    designSize: const Size(412, 570),
+  //    builder: (context, child) {
+  //      return const MaterialApp(
+  //        debugShowCheckedModeBanner: false,
+  //        home: HomeStudentInit(),
+  //      );
+  //    },
+  //  );
+  //}
+
+  // SELECCION DE USUARIO
+  
+  //@override
+  //Widget build(BuildContext context) {
+  //  return ScreenUtilInit(
+  //    designSize: const Size(412, 570), // ?
+  //    builder: (context, child) {
+  //      return const MaterialApp(
+  //        debugShowCheckedModeBanner: false,
+  //        home: SelectUser(),
+  //      );
+  //    },
+  //  );
+  //}
+
+  // HOME DEL PROFESOR
+
+  //@override
+  //Widget build(BuildContext context) {
+  //  return const MaterialApp(
+  //    //debugShowCheckedModeBanner: false,
+  //    home: TeacherHome(),
+  //  );
+  //}
 
   // FORMULARIO USUARIO/ALUMNO
   //
@@ -52,16 +158,15 @@ class MainApp extends StatelessWidget {
   // su mapa será {'Audio':false,'Pictogramas':false,'Texto':true}, el índice es dentro de la lista que le pasamos,
   // la lista de clases es ['3A','2A','1A'] y el alumno esta en 2A, el indice que pasamos es 1
 
-  /*@override
-  Widget build(BuildContext context) {
-   return const FormularioAlumnos(
-      'Añadir usuario',
-     'Agustin',
-    '123',
-   {'Audio': true, 'Pictogramas': false, 'Texto': false},
-  ['1A', '2A', '3A'],
-   2,
-   '',
-   0);
-  }*/
+  //@override
+  //Widget build(BuildContext context) {
+  // return const FormularioAlumnos(
+  //    'Añadir alumno',
+  //   'Agustin',
+  //  '123',
+  // {'Audio': true, 'Pictogramas': false, 'Texto': false},
+  //['1A', '2A', '3A'],
+  // 2,
+  // '');
+  // }
 }
--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -3,11 +3,12 @@ import 'dart:convert';
 
 import 'package:appcesible/models/user_model.dart';
 
-String _baseAddress = '10.0.2.2:8080';
+//String baseAdresss = = '192.168.1.136:8080';  // IP ordenador (para usar la app desde el movil)
+String baseAddress = 'localhost:8080';
 
 void createUser(UserModel user, String password) async {
   final response = await http.post(
-    Uri.http(_baseAddress, '/user/new'),
+    Uri.http(baseAddress, '/user/new'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8'
     },
@@ -23,26 +24,53 @@ void createUser(UserModel user, String password) async {
 
   if (response.statusCode == 200) {
     print("Updated user");
-  }
-  else {
+  } else {
     throw Exception('Failed to update User');
   }
 }
 
 // Function that makes a HTTP request to get a User from the server DB
 Future<UserModel> getUserFromId(int id) async {
-  final response = await http.get(Uri.http(_baseAddress, '/user/$id'),
+  final response = await http.get(Uri.http(baseAddress, '/user/$id'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8'
       });
 
   if (response.statusCode == 200) {
     dynamic json = jsonDecode(response.body);
-    print(json);
     return UserModel.fromJSON(json);
-  }
-  else {
+  } else {
     throw Exception('Failed to load User');
+  }
+}
+
+// Return the number of users in the DB
+Future<int> countUsers() async {
+  final response = await http.get(Uri.http(baseAddress, '/user'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      });
+
+  if (response.statusCode == 200) {
+    List<dynamic> userList = jsonDecode(response.body);
+    return userList.length;
+  } else {
+    throw Exception('Failed to fetch user list');
+  }
+}
+
+// Return all users information
+Future<List<UserModel>> getAllUsers() async {
+  final response = await http.get(Uri.http(baseAddress, '/user'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      });
+
+  if (response.statusCode == 200) {
+    List<dynamic> userList = jsonDecode(response.body);
+    return userList.map((json) => UserModel.fromJSON(json)).toList();
+  } else {
+    throw Exception('Failed to fetch user list');
   }
 }
 
@@ -50,7 +78,7 @@ Future<UserModel> getUserFromId(int id) async {
 void updateUser(UserModel user, String password) async {
   int id = user.id;
   final response = await http.post(
-    Uri.http(_baseAddress, '/user/$id'),
+    Uri.http(baseAddress, '/user/$id'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8'
     },
@@ -63,71 +91,14 @@ void updateUser(UserModel user, String password) async {
 
   if (response.statusCode == 200) {
     print("Updated user");
-  }
-  else {
+  } else {
     throw Exception('Failed to update User');
-  }
-}
-
-// Future<UserModel> authenticateUser(int id, String enteredPassword) async {
-//   final response = await http.get(Uri.http(_baseAddress, '/user/$id'),
-//       headers: <String, String>{
-//         'Content-Type': 'application/json; charset=UTF-8'
-//       });
-//   if (response.statusCode == 200) {
-//     dynamic json = jsonDecode(response.body);
-//     UserModel user = UserModel.fromJSON(json);
-//     if (verifyPassword(enteredPassword, user.passwd)) {
-//       print("Contraseña correcta");
-//       return UserModel.fromJSON(json);
-//     } else {
-//       print(enteredPassword.toString());
-//       print(user.passwd.toString());
-//       throw Exception('Contraseña incorrecta');
-//     }
-//   } else {
-//     print('object');
-//     throw Exception('Failed to load User');
-//   }
-// }
-
-// bool verifyPassword(String enteredPassword, String dbPassword) {
-//   return enteredPassword == dbPassword;
-// }
-
-Future<bool> authenticateUser(UserModel user, String enteredPassword) async {
-  try {
-    final response = await http.post(
-      Uri.http(_baseAddress, '/session/login'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8'
-      },
-      body: jsonEncode(<String, dynamic>{
-        'userName': user.userName,
-        'passwd': enteredPassword,
-        'publicKey': 123,
-      }),
-    )
-    .timeout(const Duration(seconds: 200));
-
-    if (response.statusCode == 200) {
-      var jsonResponse = await json.decode(json.encode(response.body));
-      print('Autenticacion exitosa. Token: $jsonResponse');
-      return true;
-    } else {
-      print(
-          'Error en la autenticación. Código de estado: ${response.statusCode}');
-      return false;
-    }
-  } catch (e) {
-    print('Error durante la solicitud: $e');
-    return false;
   }
 }
 
 void createStep(String media, String description) async {
   final response = await http.post(
-    Uri.http(_baseAddress, '/task/step/new'),
+    Uri.http(UserModel.baseAddress, '/task/step/new'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8'
     },
@@ -143,35 +114,5 @@ void createStep(String media, String description) async {
     print("New step created");
   } else {
     throw Exception('Failed to create a step');
-  }
-}
-
-Future<bool> pictoAuthenticateUser0(UserModel user, String enteredPassword) async {
-  try {
-    final response = await http.post(
-      Uri.http(_baseAddress, '/session/login'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8'
-      },
-      body: jsonEncode(<String, dynamic>{
-        'userName': user.userName,
-        'passPart0': enteredPassword,
-        'publicKey': 123,
-      }),
-    )
-    .timeout(const Duration(seconds: 200));
-
-    if (response.statusCode == 200) {
-      var jsonResponse = await json.decode(json.encode(response.body));
-      print('Autenticacion exitosa. Token: $jsonResponse');
-      return true;
-    } else {
-      print(
-          'Error en la autenticación. Código de estado: ${response.statusCode}');
-      return false;
-    }
-  } catch (e) {
-    print('Error durante la solicitud: $e');
-    return false;
   }
 }
