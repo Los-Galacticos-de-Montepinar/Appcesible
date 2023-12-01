caaa5b83b512b8c6436a99938ed5647e99e640b0
pabloqr <pabloquesadarojo02@gmail.com>
2023-12-01 11:42:54 +0100

    added script to update branches

---
+++ pullBranches.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+ehco
+echo "Actualizando la rama main local..."
+echo
+git checkout main
+git pull
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/baja-usuario con la rama main remota..."
+echo
+git checkout feature/baja-usuario
+git pull
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/control-sesion con la rama main remota..."
+echo
+git checkout feature/control-sesion
+git pull
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/form-usuario con la rama main remota..."
+echo
+git checkout feature/form-usuario
+git pull
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/seleccionar-tarea con la rama main remota..."
+echo
+git checkout feature/seleccionar-tarea
+git pull
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/seleccionar-usuario con la rama main remota..."
+echo
+git checkout feature/seleccionar-usuario
+git pull
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/tarea-fija con la rama main remota..."
+echo
+git checkout feature/tarea-fija
+git pull
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/tarea-material con la rama main remota..."
+echo
+git checkout feature/tarea-material
+git pull
+echo
+echo "...actualizada"
+echo
+
+
+echo
+echo "Actualizadas todas las ramas con la rama main remota!"
+echo
+git checkout main
---
+++ updateBranches.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+ehco
+echo "Actualizando la rama feature/baja-usuario con la rama main local..."
+echo
+git checkout feature/baja-usuario
+git merge main
+git push
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/control-sesion con la rama main local..."
+echo
+git checkout feature/control-sesion
+git merge main
+git push
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/form-usuario con la rama main local..."
+echo
+git checkout feature/form-usuario
+git merge main
+git push
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/seleccionar-tarea con la rama main local..."
+echo
+git checkout feature/seleccionar-tarea
+git merge main
+git push
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/seleccionar-usuario con la rama main local..."
+echo
+git checkout feature/seleccionar-usuario
+git merge main
+git push
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/tarea-fija con la rama main local..."
+echo
+git checkout feature/tarea-fija
+git merge main
+git push
+echo
+echo "...actualizada"
+echo
+
+ehco
+echo "Actualizando la rama feature/tarea-material con la rama main local..."
+echo
+git checkout feature/tarea-material
+git merge main
+git push
+echo
+echo "...actualizada"
+echo
+
+
+echo
+echo "Actualizadas todas las ramas con la rama main local!"
+echo
+git checkout main
