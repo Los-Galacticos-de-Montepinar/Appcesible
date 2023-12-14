844c3938fa0acd14056f039e6e3f60d05ac3c955
pabloqr <pabloquesadarojo02@gmail.com>
2023-12-14 11:20:37 +0100

    fixes for merge v3

--- lib/services/task_service.dart
+++ lib/services/task_service.dart
@@ -4,9 +4,9 @@ import 'dart:convert';
 
 import 'package:appcesible/models/task_model.dart';
 
-// String _baseAddress = '10.0.2.2:8080';      // IP emulador
+String _baseAddress = '10.0.2.2:8080';      // IP emulador
 // String _baseAddress = 'localhost:8080';
-String _baseAddress = '192.168.1.42:8080';  // IP ordenador
+// String _baseAddress = '192.168.1.42:8080';  // IP ordenador
 
 // CREATE
 
@@ -17,8 +17,8 @@ Future<int> createTask(TaskModel task) async {
       'Content-Type': 'application/json; charset=UTF-8'
     },
     body: jsonEncode(<String, dynamic>{
-      'desc': task.description,
       'title': task.title,
+      'desc': task.description
     }),
   );
 
